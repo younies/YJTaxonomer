833b45c7df827e3dc7c623df49dc31b46eaab363
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-07-19 12:56:12 -0500

    final_commit I hope

--- YJTaxonomer/configuration.h
+++ YJTaxonomer/configuration.h
@@ -17,7 +17,7 @@ using namespace std;
 string kmer_length_str = "31";
 
 LONGS random_number_size = 10000000;
-string path_to_random_numbers = "";
+string path_to_random_numbers = "/export/project/hondius/yr_db/for_garbages/tenMillionRandom.txt";
 
 //names
 string name_of_the_databases = "databases";
@@ -25,11 +25,11 @@ string name_of_output_results = "outputResults" +kmer_length_str + ".txt";
 
 
 //pathes
-string path_to_the_program_directory = "";
-string path_to_the_genomes_database = "";
-string path_to_the_names_dmp_file = "/Users/youniesmahmoud/Desktop/research/taxdump/names.txt";
-string path_to_the_nodes_dmp_file = "/Users/youniesmahmoud/Desktop/research/taxdump/nodes.txt";
-string path_to_the_specific_nodes = "/Users/youniesmahmoud/Desktop/research/newNmaes.txt";
+string path_to_the_program_directory = "/export1/project/hondius/newKrakenResearch/";
+string path_to_the_genomes_database = "/export1/project/hondius/newKrakenResearch/databases/kmerDatabase31/";
+string path_to_the_names_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/names.txt";
+string path_to_the_nodes_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/nodes.txt";
+string path_to_the_specific_nodes = "/export1/project/hondius/newKrakenResearch/databases/uids.txt";
 
 
 
