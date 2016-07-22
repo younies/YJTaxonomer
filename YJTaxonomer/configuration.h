7ee7d22ef2ce18ae9cefa83d8c52f79676651292
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-07-22 06:49:44 -0500

    no message

--- YJTaxonomer/configuration.h
+++ YJTaxonomer/configuration.h
@@ -21,15 +21,15 @@ string path_to_random_numbers = "/export/project/hondius/yr_db/for_garbages/tenM
 
 //names
 string name_of_the_databases = "databases";
-string name_of_output_results = "outputResults" +kmer_length_str + "_withKmers_withUIDS.txt";
+string name_of_output_results = "outputResults_newUIDs" +kmer_length_str + "_withKmers_withUIDS.txt";
 
 
 //pathes
 string path_to_the_program_directory = "/export1/project/hondius/newKrakenResearch/";
-string path_to_the_genomes_database = "/export1/project/hondius/newKrakenResearch/databases/kmerDatabase31/";
+string path_to_the_genomes_database = "/export1/project/hondius/newKrakenResearch/databases/kmerDatabase_new_31/";
 string path_to_the_names_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/names.txt";
 string path_to_the_nodes_dmp_file = "/export1/project/hondius/newKrakenResearch/databases/nodes.txt";
-string path_to_the_specific_nodes = "/export1/project/hondius/newKrakenResearch/databases/uids.txt";
+string path_to_the_specific_nodes = "/export1/project/hondius/newKrakenResearch/databases/uids_new.txt";
 
 
 
